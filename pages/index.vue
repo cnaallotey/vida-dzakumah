e499caec72ddfdaaab669867b3c077d254fce0b8
Charles Nii Adotey Allotey <charlesallotey1995@gmail.com>
2024-08-09 09:03:14 +0000

    download feature

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 
 <template>
-    <div class="w-full min-h-screen flex items-center justify-center p-4">
+    <div class="w-full min-h-screen flex items-center justify-center px-4 py-10">
         <div class="w-fit flex flex-col items-center gap-5">
-        <img src="/images/pre-title.svg" class="w-[300px]" alt="title">
+        <img src="/images/pre-title.svg" class="w-60 md:w-[300px]" alt="title">
         <img src="/images/cover.png" class="w-[500px] h-fit" alt="title">
-        <img src="/images/name.svg" class="w-[600px] h-fit" alt="title">
+        <img src="/images/name.svg" class="w-80 md:w-[500px] h-fit" alt="title">
         <div class="flex flex-col w-full md:flex-row items-center gap-4">
-        <UButton to="/brochure" class=" flex w-full md:px-32 md:w-fit items-center justify-center h-20 text-xl bg-white text-[#E7AF3E] hover:bg-white/80">View Brochure</UButton>
-        <a href="/brochure.pdf" target="_blank"  class="w-full md:px-32 md:w-fit flex items-center justify-center rounded-md text-white font-medium h-20 text-xl bg-[#E7AF3E] hover:bg-[#E7AF3E]/80">Download Brochure</a>
+        <NuxtLink to="/brochure" class=" flex rounded-md w-full md:px-32 md:w-fit items-center justify-center h-14 md:h-20 md:text-xl bg-white text-[#E7AF3E] hover:bg-white/80">View Brochure</NuxtLink>
+        <a href="https://drive.google.com/uc?export=download&id=1_HJn2MMI4gcTM79ml5xJWvIomVrIbnLT" target="_blank"  class="w-full md:px-32 md:w-fit flex items-center justify-center h-14 rounded-md text-white font-medium md:h-20 md:text-xl bg-[#E7AF3E] hover:bg-[#E7AF3E]/80">Download Brochure</a>
         </div>
         </div>
     </div>
