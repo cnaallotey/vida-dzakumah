a55918af7891931b81d8028d67bc8b9517a09215
Charles Nii Adotey Allotey <charlesallotey1995@gmail.com>
2024-08-09 08:50:14 +0000

    download feature

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
         <img src="/images/name.svg" class="w-[600px] h-fit" alt="title">
         <div class="flex flex-col w-full md:flex-row items-center gap-4">
         <UButton to="/brochure" class=" flex w-full md:px-32 md:w-fit items-center justify-center h-20 text-xl bg-white text-[#E7AF3E] hover:bg-white/80">View Brochure</UButton>
-        <a href="/brochure.pdf"  class="w-full md:px-32 md:w-fit flex items-center justify-center rounded-md text-white font-medium h-20 text-xl bg-[#E7AF3E] hover:bg-[#E7AF3E]/80">Download Brochure</a>
+        <a href="/brochure.pdf" target="_blank"  class="w-full md:px-32 md:w-fit flex items-center justify-center rounded-md text-white font-medium h-20 text-xl bg-[#E7AF3E] hover:bg-[#E7AF3E]/80">Download Brochure</a>
         </div>
         </div>
     </div>
